2c2563870f6cb49901d744216aa483327e7288a5
Timothy <yexiaozhou2003@gmail.com>
2018-04-10 17:52:54 +0800

    add get room by id interface

--- src/go-chess-api/resources/room.py
+++ src/go-chess-api/resources/room.py
@@ -2,6 +2,7 @@ from flask_restful import Resource
 import config
 import config.app_config as app_config
 from flask import jsonify
+from bson.objectid import ObjectId
 
 
 class RoomList(Resource):
@@ -18,4 +19,11 @@ class RoomList(Resource):
 
 class Room(Resource):
     def get(self, room_id):
-        return 'room_id:' + room_id
+        data = []
+        db = config.mongoDBHelper(app_config.MONGODB_NAME)
+        room = db.rooms.find_one({"_id": ObjectId(room_id)})
+        if room is not None:
+            room['_id'] = str(room['_id'])
+            data.append(room)
+        resp = jsonify({"code": 0, "message": "success", "data": data})
+        return resp
