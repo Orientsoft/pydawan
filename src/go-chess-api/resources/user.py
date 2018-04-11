0e340213b1d42b031c301f0ca8811133d7dcf9b7
Timothy <yexiaozhou2003@gmail.com>
2018-04-11 10:31:08 +0800

    add user list interface

--- src/go-chess-api/resources/user.py
+++ src/go-chess-api/resources/user.py
@@ -1,9 +1,19 @@
 from flask_restful import Resource
+import config
+import config.app_config as app_config
+from flask import jsonify
 
 
 class UserList(Resource):
     def get(self):
-        return 'room_list'
+        data = []
+        db = config.mongoDBHelper(app_config.MONGODB_NAME)
+        users = db.users.find({})
+        for user in users:
+            user['_id'] = str(user['_id'])
+            data.append(user)
+        resp = jsonify({'code': 0, 'message': 'success', 'data': data})
+        return resp
 
 
 class User(Resource):
