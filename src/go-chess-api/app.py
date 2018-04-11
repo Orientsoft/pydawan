a3aa88f642d02d523d784804b59265bb33dbf43b
Timothy <yexiaozhou2003@gmail.com>
2018-04-11 10:11:26 +0800

    comment JWT auth for debug purpose

--- src/go-chess-api/app.py
+++ src/go-chess-api/app.py
@@ -16,21 +16,19 @@ api.add_resource(Room, '/rooms/<room_id>')
 api.add_resource(UserList, '/users')
 api.add_resource(User, '/users/<user_id>')
 
-
-@app.before_request
-def pre_auth():
-    if 'Authorization' in request.headers:
-        auth_jwt = request.headers['Authorization']
-        if auth_jwt != '':
-            payload = jwt.decode(
-                auth_jwt, app_config.JWT_SECRET, algorithms=['HS256'])
-            # Store user id in g
-            g.user_id = payload['user_id']
-        else:
-            abort(403)
-    else:
-        abort(403)
-
+# @app.before_request
+# def pre_auth():
+#     if 'Authorization' in request.headers:
+#         auth_jwt = request.headers['Authorization']
+#         if auth_jwt != '':
+#             payload = jwt.decode(
+#                 auth_jwt, app_config.JWT_SECRET, algorithms=['HS256'])
+#             # Store user id in g
+#             g.user_id = payload['user_id']
+#         else:
+#             abort(403)
+#     else:
+#         abort(403)
 
 if __name__ == '__main__':
     app.run(debug=True)
